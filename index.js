d34a096fc7ee209d96f48f20248c0166ddd39907
Dan Hintz <dhintz@live.com>
2019-06-22 21:39:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function lowerCaseDrivers(arr) {
 function nameToAttributes(arr) {
   return arr.map(function (driver) {return {firstName: driver.split(' ')[0], lastName: driver.split(' ')[1]};});
 }
+
+function attributesToPhrase(arr) {
+  return arr.map(function (driver) {return `${driver.name} is from ${driver.hometown}`});
+}
